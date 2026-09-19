1f02cb8e06400291f90ee34fb349c67993431e6d
phemotrix <phemotrix@gmail.com>
2026-09-20 00:46:49 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,106 @@
+const CACHE_NAME = "review-experience-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+
+/* =========================
+   INSTALL
+   ========================= */
+
+self.addEventListener("install", (event) => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+
+        return cache.addAll(FILES_TO_CACHE);
+
+      })
+
+  );
+
+  self.skipWaiting();
+
+});
+
+
+/* =========================
+   ACTIVATE
+   ========================= */
+
+self.addEventListener("activate", (event) => {
+
+  event.waitUntil(
+
+    caches.keys()
+      .then((cacheNames) => {
+
+        return Promise.all(
+
+          cacheNames
+            .filter(
+              (name) => name !== CACHE_NAME
+            )
+            .map(
+              (name) => caches.delete(name)
+            )
+
+        );
+
+      })
+
+  );
+
+  self.clients.claim();
+
+});
+
+
+/* =========================
+   FETCH
+   ========================= */
+
+self.addEventListener("fetch", (event) => {
+
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+
+    fetch(event.request)
+      .then((response) => {
+
+        const responseClone =
+          response.clone();
+
+        caches.open(CACHE_NAME)
+          .then((cache) => {
+
+            cache.put(
+              event.request,
+              responseClone
+            );
+
+          });
+
+        return response;
+
+      })
+
+      .catch(() => {
+
+        return caches.match(
+          event.request
+        );
+
+      })
+
+  );
+
+});
